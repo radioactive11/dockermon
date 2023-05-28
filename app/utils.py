d36565ad4ae5325cbcf35891ae347b7224de529d
radioactive11 <roy.arijit2001@gmail.com>
2023-05-28 20:04:16 +0530

    merging as plain string without formatting

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,9 @@
+import os
+
 from logs import Logs
 from stats import Stats
 
 
-def merge_streams(_log: Logs, _stat: Stats):
+def merge_streams(_log: Logs, _stat: Stats, save: bool = False):
     for log, stat in zip(_log.stream_logs(), _stat.stream_stats()):
-        # yield log, stat
-        yield f"data: {log}\ndata: {stat}\n\n"
+        yield f"{stat}|{log}"
